751371f125a80451e2bc86d5124f1371eb7fd11f
Hernán Valdés <hfvaldesg@gmail.com>
2021-01-16 11:17:58 +0100

    Produce the example 1 from Wikipedia

--- include/lsystem.hpp
+++ include/lsystem.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
+#include <vector>
+#include <map>
+#include <string>
+
 class LSystem {
    private:
+    std::vector<std::string> variables;
+    std::string axiom;
+    std::map<std::string, std::string> rules;
+    void produce();
    public:
     LSystem();
     void draw();
--- src/lsystem.cpp
+++ src/lsystem.cpp
@@ -3,11 +3,34 @@
 #include <opencv4/opencv2/core.hpp>
 #include <opencv4/opencv2/highgui.hpp>
 #include <opencv4/opencv2/imgproc.hpp>
+#include <iostream>
 
 LSystem::LSystem() {
+    this->variables = {"A", "B"};
+    this->axiom = "A";
+    this->rules.insert({"A", "AB"});
+    this->rules.insert({"B", "A"});
+}
+
+void LSystem::produce() {
+    std::string output = this->axiom;
+    std::cout << "n = " << 0 << " : " << output << std::endl;
+    
+    for (int n = 1; n < 8; n++) {
+        std::string tmp_output = "";
+        for (auto variable_char : output) {
+            std::string variable_str{variable_char};
+            tmp_output += this->rules.at(variable_str);
+        }
+
+        std::cout << "n = " << n << " : " << tmp_output << std::endl;
+        output = tmp_output;
+    }
 }
 
 void LSystem::draw() {
+    this->produce();
+
     // Create Img with a uniform background color
     cv::Mat img(500, 500, CV_8UC3, cv::Scalar(255, 255, 255));
 
