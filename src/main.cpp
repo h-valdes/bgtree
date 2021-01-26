2e9bed5637d3ef8c346fb498f6ef29f58bb8d0fc
Hernán Valdés <hfvaldesg@gmail.com>
2021-01-26 11:46:25 +0100

    Make mini functionality for the parse of arguments

--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,24 @@
 #include <memory>
 #include <string>
 
-#include "CLI/CLI.hpp"
 #include "user.hpp"
 
 int main(int argc, char** argv) {
-    CLI::App app;
+    std::string config_path;
+
+    if (argc >= 2) {
+        config_path = argv[1];
+    } else {
+        return EXIT_FAILURE;
+    }
+
+    std::cout << config_path << std::endl;
 
     auto user_info = std::make_shared<UserInfo>();
+    user_info->is_preview = true;
 
-    // Subcommand draw
-    auto draw_sub = app.add_subcommand("draw", "Draw");
-
-    // preview flag
-    bool flag_preview;
-    auto preview_flag = draw_sub->add_flag(
-        "--preview, -p", flag_preview, "Display the background with ImageMagick");
-
-    // Path of the output file
-    std::string output_path;
-    auto output_option = draw_sub->add_option(
-        "--output, -o", output_path, "Path to the output file");
-
-    // Path of the config file
-    std::string config_path;
-    auto config_option = draw_sub->add_option(
-        "--config, -c", config_path, "Path to the config file");
-
-    draw_sub->callback([&]() {
-        if (*config_option)
-            user_info->read_config(config_path);
-
-        if (*preview_flag)
-            user_info->is_preview = flag_preview;
-
-        if (*output_option)
-            user_info->general_info.output_file = output_path;
-
-        user_info->draw();       
-    });
-
-    CLI11_PARSE(app, argc, argv);
+    user_info->read_config(config_path);
+    user_info->draw();
 
     return EXIT_SUCCESS;
 }
